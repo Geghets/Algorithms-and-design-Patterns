ab4b9b463320f525a53ddfe762a0119132fa91f7
Geghets <geghecik.balayan@gmail.com>
2020-03-30 22:45:57 +0400

    Changed

--- binary-search.js
+++ binary-search.js
@@ -1,30 +1,29 @@
-let arr = [1, 8, 2, 6, 7, 9, 5, 4];
+function binarySearch (sortedArray, searchValue) {
 
-let neededNumber = 5;
+    let start = 0;
+
+    let end = arr.length - 1;
+
+    let foo = true;
+
+    while (foo === true) {
+        let halfLength = Math.floor((end + start) / 2);
+
+        if(sortedArray[halfLength] > searchValue) {
+            end = halfLength - 1;
+        } else if (sortedArray[halfLength] < searchValue) {
+            start = halfLength + 1;
+        }
+        else {
+            foo = false;
+            console.log('index of ' + searchValue + ' is ' + halfLength);
+        }
+    }
+
+}
+
+let arr = [1, 8, 2, 6, 7, 9, 5, 4];
 
 arr = arr.sort();
 
-console.log(arr);
-
-let begin = 0;
-
-let end = arr.length - 1;
-
-console.log(end);
-
-let foo = true;
-
-
-while (foo === true) {
-  let halfLength = Math.floor((end + begin) / 2);
-
-  if (arr[halfLength] > neededNumber) {
-      end = halfLength;
-  } else if (arr[halfLength] < neededNumber) {
-      begin = halfLength
-  }
-  else {
-      foo = false;
-      console.log('index of ' + neededNumber + ' is ' + halfLength);
-  }
-}
+binarySearch(arr, 5);
